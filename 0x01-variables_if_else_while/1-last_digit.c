0c910978c165dd2a7f5947a26d6a99ed432d23f9
adedamolagig <adedamolagig@gmail.com>
2023-06-16 09:20:35 -0700

    corrected code to meet Betty code style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,8 +6,7 @@
 /**
  * main - Entry point
  *
- * Description: print the value of n status:
- * 	greater than, is zero and is not less than 6.
+ * Description: print the value of n status: greater than, is zero and is not less than 6.
  *
  * Return: Always 0 (Success)
  */
@@ -25,7 +24,7 @@ int main(void)
 		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
 	else if (digit == 0)
 		printf("Last digit of %i is %i and is 0\n", n, digit);
-	else if (digit < 6 && digit !=0)
+	else if (digit < 6 && digit != 0)
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
 	return (0);
 }
