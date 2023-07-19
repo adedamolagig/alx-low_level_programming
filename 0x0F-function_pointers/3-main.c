1f22eb71167a3354d0d2432e81178c1f0603f862
adedamolagig <adedamolagig@gmail.com>
2023-07-19 10:44:01 -0700

    a program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry point of the program.
+ * @argc: Number of arguments passed to the program.
+ * @argv: Array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int (*get_op_func(char *s))(int, int);
+int main(int argc, char *argv[])
+{
+    int num1, num2, result;
+    int (*op_func)(int, int);
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+    op_func = get_op_func(argv[2]);
+
+    if (op_func == NULL)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    result = op_func(num1, num2);
+    printf("%d\n", result);
+
+    return (0);
+}
+
