6fab3cb32bca507f2c211e62347943b2d5ad20b7
adedamolagig <adedamolagig@gmail.com>
2023-09-02 16:23:31 -0700

    all files needed created

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+/**
+ * _memset - about function
+ * @s: pointer to a character variable
+ * @b: another character variable
+ * @n: an unsigned integer
+ */
+char *_memset(char *s, char b, unsigned int n){
+
+}
