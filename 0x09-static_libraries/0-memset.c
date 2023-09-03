faf9bca75ee69a714153b92d5cbe340ebf4b614f
adedamolagig <adedamolagig@gmail.com>
2023-09-03 14:19:01 -0700

    still committing the codes in the individual files

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -2,11 +2,21 @@
 
 
 /**
- * _memset - about function
- * @s: pointer to a character variable
- * @b: another character variable
- * @n: an unsigned integer
+ * *_memset - fills memory with a constant byte.
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy character
+ *
+ * Return: pointer to the memory area s
  */
-char *_memset(char *s, char b, unsigned int n){
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b
+	}
+
+	return (s);
 }
