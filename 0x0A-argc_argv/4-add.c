112f8f5faef1a1e7b75f6668536bb9693ef2a798
adedamolagig <adedamolagig@gmail.com>
2023-09-10 06:23:35 -0700

    adding positive numbers now work as expected

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,16 +12,46 @@ bool is_positive_number(const char *str)
 {
 	if (*str == '\0')
 	{
-		return (1);
+		return (0);
 	}
 
 	while (*str)
 	{
 		if (*str < '0' || *str > '9')
 		{
-			return (1);
+			return (0);
 		}
 		str++;
 	}
+	return (1);
+}
+/**
+ * main - Entry point.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_positive_number(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
