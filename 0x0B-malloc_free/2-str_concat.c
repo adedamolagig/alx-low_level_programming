ca93a479dfd59a24474cde7d5d0e5cc73f9ea1d7
adedamolagig <adedamolagig@gmail.com>
2023-09-10 16:29:21 -0700

    concatenate two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: first string to be concatenated.
+ * @s2: second string to be concatenated.
+ *
+ * Return: pointer to the newly allocated space in memory
+ * containing the concatenated strings, or NULL on failure.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concatenated_str;
+	int s1_len = 0, s2_len = 1, i, j;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	/* Calculate the lengths of s1 and s2 */
+	while (s1[s1_len])
+	{
+		s1_len++;
+	}
+	while (s2[s2_len])
+	{
+		s2_len++;
+	}
+	/* Allocate memory for the concatenates string, including space for the null terminator */
+	concatented_str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+
+	/* Check if memory allocated failed */
+	if (concatenated_str == NUL)
+	{
+		return (NULL);
+	}
+	/* Copy the characters from s1 to concatenated_str */
+	for (i = 0; i < s1_len; i++)
+	{
+		concatenated_str[i] = s1[i];
+	}
+
+	/*Concatenate the characters from s2 to concatenated_str */
+	for (j = 0; j < s2_len; j++)
+	{
+		concatenated_str[i] = s2[j];
+		i++;
+	}
+	/* add null terminator to the end of the concatenates string*/
+	concatenated_str[i] = '\0';
+	return (concatenated_str);
+}
