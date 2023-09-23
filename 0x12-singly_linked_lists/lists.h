23a5667c5f1f2d87c194cbd7eab0b0620c274277
adedamolagig <adedamolagig@gmail.com>
2023-09-23 05:56:29 -0700

    fixing lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,16 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
-
 /**
- * struct list_s - singly linked list.
- * @str: string - (malloc'ed string).
- * @len: length of the string.
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
  *
- *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
@@ -19,9 +17,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/* Function prototype for print_list */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
-
-#endif /* MAIN_H */
+#endif
