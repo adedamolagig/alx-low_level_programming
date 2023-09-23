baaaf0e4f21d53e5495f86ada0e6ae0911a47c93
adedamolagig <adedamolagig@gmail.com>
2023-09-23 06:36:44 -0700

    asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,27 +1,9 @@
-section .data
-    hello_fmt db "Hello, Holberton",0   ; Null-terminated format string
-    newline db 10                      ; ASCII code for newline character (0xA)
-
-section .text
-    global main
-
+          global    main
+          extern    printf
 main:
-    ; Call printf with the format string
-    mov rdi, hello_fmt
-    call printf
-
-    ; Print a newline character
-    mov rdi, newline
-    call putchar
-
-    ; Exit the program
-    mov rax, 60         ; syscall number for exit
-    xor rdi, rdi        ; exit status (0)
-    syscall
-
-section .bss
-    hello_fmt resb 256  ; Reserve space for format string (256 bytes)
-
-section .extern
-    extern printf, putchar
-
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
