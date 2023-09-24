dfa84b169a58f46255df09268bff67389d56e0a4
adedamolagig <adedamolagig@gmail.com>
2023-09-24 06:51:42 -0700

    opcodes pass

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,39 +2,41 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point of the program
- * @argc: The number of command-line arguments
- * @argv: An array of command-line argument strings
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-    int i, num_bytes;
+	int bytes, i;
+	char *arr;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        exit(1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-    num_bytes = atoi(argv[1]);
+	bytes = atoi(argv[1]);
 
-    if (num_bytes < 0)
-    {
-        printf("Error\n");
-        exit(2);
-    }
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-    unsigned char *opcode_ptr = (unsigned char *)main;
+	arr = (char *)main;
 
-    for (i = 0; i < num_bytes - 1; i++)
-    {
-        printf("%02x ", opcode_ptr[i]);
-    }
-
-    printf("%02x\n", opcode_ptr[i]); // Print the last opcode with a new line
-
-    return (0);
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
 }
-
