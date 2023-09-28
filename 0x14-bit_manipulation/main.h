57f7a5f6b5864cbf24e9b0109e6b2898b834d86f
adedamolagig <adedamolagig@gmail.com>
2023-09-28 04:53:23 -0700

    fixing debugging error in task

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,7 @@
 /* Function prototype */
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 /* Function prototype for _putchar */
 int _putchar(char c);
